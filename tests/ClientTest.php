c72e06182fe8562d517b58cb7940b3080d5fe1ac
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-09-23 09:48:53 -0700

    Add a passing test to the client class for the deleteAll function

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -106,5 +106,29 @@
 
         }
 
+        function test_deleteAll()
+        {
+            //Arrange
+            $id = 1;
+            $name = "ally";
+            $stylist_id = 3;
+            $client = new Client($id, $name, $stylist_id);
+            $client->save();
+
+            $id = 2;
+            $name = "cally";
+            $stylist_id = 4;
+            $client2 = new Client($id, $name, $stylist_id);
+            $client2->save();
+
+            //Act
+            $result = $client->deleteAll();
+
+            //Assert
+            $result = $client->getAll();
+            $this->assertEquals([], $result);
+
+        }
+
     }
  ?>
