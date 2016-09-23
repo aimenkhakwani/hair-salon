e9994c49a59d0ef8d89c5625751f1dcd31f73021
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-09-23 09:24:29 -0700

    Add methods and passing tests for name and id getters and setters

---
+++ src/Stylist.php
@@ -0,0 +1,78 @@
+<?php
+    Class Stylist
+    {
+        private $id;
+        private $name;
+
+        function __construct($id = null, $name)
+        {
+            $this->id = $id;
+            $this->name = $name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setName($name)
+        {
+            $this->name = (string) $name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function update($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE stylists SET name = '{new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
+        function delete($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE stylists SET name = '{new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            $stylists = array();
+            foreach($returned_stylists as $stylist){
+                $id = $stylist['id'];
+                $name = $stylist['name'];
+                $new_stylist = new Stylist($id, $name);
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+
+        static function find($search_id)
+        {
+            $found_stylist = null;
+            $stylists = Stylist::getAll();
+            foreach($stylists as $stylist) {
+                $stylist_id = $stylist->getId();
+                if($stylist_id == $search_id) {
+                    $found_stylist = $stylist;
+                }
+            }
+            return $found_stylist;
+        }
+    }
+
+?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,149 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Stylist::deleteAll();
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $id = 22;
+            $name = "Sally";
+            $stylist = new Stylist($id, $name);
+
+            //Act
+            $result = $stylist->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+
+            //Act
+            $result = $stylist->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+            $stylist->save();
+
+            //Act
+            $result = $stylist->getAll();
+
+            //Assert
+            $this->assertEquals($stylist, $result[0]);
+
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+            $stylist->save();
+
+            $id = 2;
+            $name = "mandy";
+            $stylist2 = new Stylist($id, $name);
+            $stylist2->save();
+
+            //Act
+            $result = $stylist->getAll();
+
+            //Assert
+            $this->assertEquals([$stylist,$stylist2], $result);
+
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+            $stylist->save();
+
+            $id = 2;
+            $name = "mandy";
+            $stylist2 = new Stylist($id, $name);
+            $stylist2->save();
+
+            //Act
+            $result = $stylist->deleteAll();
+
+            //Assert
+            $result = $stylist->getAll();
+            $this->assertEquals([], $result);
+
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+            $stylist->save();
+
+            $id = 2;
+            $name = "mandy";
+            $stylist2 = new Stylist($id, $name);
+            $stylist2->save();
+
+            //Act
+            $result = Stylist::find($stylist->getId());
+
+            //Assert
+            $this->assertEquals($stylist, $result);
+
+        }
+
+        function test_update  ()
+        {
+            //Arrange
+            $id = 1;
+            $name = "sally";
+            $stylist = new Stylist($id, $name);
+            $stylist->save();
+
+            $new_name = "hansen";
+
+            //Act
+            $stylist->update($new_name);
+
+            //Assert
+            $this->assertEquals($new_name, $stylist->getName());
+
+        }
+    }
+ ?>
